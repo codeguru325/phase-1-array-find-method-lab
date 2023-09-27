07f2ccd7621f65a5f7ead8d1d02738030d707d96
samuel <sammykuria300@gmail.com>
2023-09-27 10:13:49 +0300

    Quick.commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  function superbowlWin(record) {
+    const winnings = record.find(function (element) {
+      return element.result === "W";
+    });
+  
+    if (winnings) {
+      return winnings.year
+    } else {
+      return undefined
+    }
+  }
+  
+ 
+
